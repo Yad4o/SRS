c5d0eab12de72d678f1671b51ae7d5c0da490c91
Om yadao <omyadao1706@gmail.com>
2026-02-20 09:02:56 +0530

    feat: task1.1 (#7)
    * feat: task1.1
    
    * fix: all issues from coderabbit
    
    * fix: coderabbit fix 2

---
+++ act.ps1
@@ -0,0 +1,11 @@
+﻿# act.ps1
+$VenvPath = ".venv\Scripts\Activate.ps1"
+
+if (-Not (Test-Path $VenvPath)) {
+    Write-Host "❌ Virtual environment not found."
+    Write-Host "👉 Run: python -m venv .venv"
+    exit 1
+}
+
+& $VenvPath
+Write-Host "✅ Virtual environment activated"
